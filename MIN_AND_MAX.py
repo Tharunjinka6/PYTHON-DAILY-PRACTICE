2d6679573aab361210d69378a2e967cf217d428d
Tharunjinka6 <tharunjinka2155@gmail.com>
2026-06-23 23:16:25 +0530

    without using built in function

--- MIN_AND_MAX.py
+++ MIN_AND_MAX.py
@@ -3,3 +3,17 @@ class Solution:
         # code here
         arr.sort()
         return([arr[0],arr[-1]])
+    
+#without using builtinfunction
+class Solution:
+    def getMinMax(self, arr):
+        # code here
+        min=arr[0]
+        max=arr[0]
+        for i in arr:
+            if i<min:
+                min=i
+            if i>max:
+                max=i
+        return [min,max]
+        
