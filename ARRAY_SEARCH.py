13ea1e95b2022211a8382843f6bb42997dc7bb87
Tharunjinka6 <tharunjinka2155@gmail.com>
2026-06-23 21:55:38 +0530

    updated without built-in function

--- ARRAY_SEARCH.py
+++ ARRAY_SEARCH.py
@@ -5,4 +5,13 @@ class Solution:
             return(arr.index(x))
         else:
             return -1
+
+# without built-in functions
+class Solution:
+    def search(self, arr, x):
+        # code here
+        for i in arr:
+            if i == x:
+                return (arr.index(x))
+        return -1
         
---
+++ array_.py
@@ -0,0 +1,7 @@
+arr=[1,2,3,4]
+x=3
+for i in arr:
+    if i==x:
+        print(arr.index(x))
+else:
+    print(-1)
