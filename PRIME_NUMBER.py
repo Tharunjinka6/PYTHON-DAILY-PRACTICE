7395f1a3b853dc05b2f121ff1c0583e0680463cb
Tharunjinka6 <tharunjinka2155@gmail.com>
2026-06-24 01:34:39 +0530

    Update PRIME_NUMBER.py

--- PRIME_NUMBER.py
+++ PRIME_NUMBER.py
@@ -2,7 +2,7 @@ class Solution:
     def isPrime(self, n):
         # code here
         if n>1:
-            if n%1==0 and n%n==0:
-                return True
-            else:
-                return False
+           for i in range(2,n):
+               if n%i==0:
+                   return False
+           return True  
