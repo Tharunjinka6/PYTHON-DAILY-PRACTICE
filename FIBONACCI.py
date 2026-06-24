e97e28a0c9570d4bc9d298eb0e1f80e11ca71163
Tharunjinka6 <tharunjinka2155@gmail.com>
2026-06-24 20:01:42 +0530

    Update FIBONACCI.py

--- FIBONACCI.py
+++ FIBONACCI.py
@@ -1,10 +1,14 @@
 class Solution:
     def nthFibonacci(self, n: int) -> int:
         # code here
+        if n==0:
+            return 0
+        if n==1:
+            return 1
         a,b=0,1
         f=0
         if n>=2:
-            for i in range(n-1):
+            for i in range(2,n+1):
                 f=a+b
                 a=b
                 b=f
