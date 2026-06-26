c3e8e7db12f3c4b302bfaf0b761bb9a9502274d3
Tharunjinka6 <tharunjinka2155@gmail.com>
2026-06-27 01:27:01 +0530

    factorial

--- Factorial1.py
+++ Factorial1.py
@@ -1,5 +1,7 @@
-n=5
-fact=1
-for i in range(1,n+1):
-    fact=fact*i
-print(fact)
+class Solution:
+    def factorial(self, n: int) -> int:
+        # code here
+        fact=1
+        for i in range(1,n+1):
+            fact=fact*i
+        return fact
