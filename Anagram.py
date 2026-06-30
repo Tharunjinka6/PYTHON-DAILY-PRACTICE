caf1c2f2c00056300d52373b30b46e9e6d068271
Tharunjinka6 <tharunjinka2155@gmail.com>
2026-06-30 23:35:20 +0530

    practice

---
+++ Anagram.py
@@ -0,0 +1,24 @@
+str1="tharun"
+str2="tahurn"
+new_str1=""
+new_str2=""
+Ascii_1=0
+Ascii_2=0
+for i in str1:
+    new_str1=new_str1+i
+    if ord(new_str1)<i:
+        
+
+#works only when the two strings have the same charcters
+# str1="tharun"
+# Ascii_1=0
+# str2="tahurn"
+# Ascii_2=0
+# for i in str1:
+#     Ascii_1=Ascii_1+ord(i)
+# for j in str2:
+#     Ascii_2=Ascii_2+ord(j)
+# if Ascii_1==Ascii_2:
+#     print("Anagram")
+# else:
+#     print("not a Anagram")
