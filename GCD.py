21f9f78fc64bd010370a19adacd8bbeccffe8f72
Tharunjinka6 <tharunjinka2155@gmail.com>
2026-07-01 00:45:50 +0530

    Update GCD.py

--- GCD.py
+++ GCD.py
@@ -1,3 +1,16 @@
+#learned it from chatgpt but time limit exceed for this method also
+class Solution:
+    def gcd(self, a, b):
+        # code here
+        gcd=1
+        small=a
+        if a>b:
+            small=b
+        for i in range(1,small+1):
+            if a%i==0 and b%i==0:
+                gcd=i
+        return gcd
+        
 #Time limit exceeded
 class Solution:
     def gcd(self, a, b):
