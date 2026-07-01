d0227a1654afe27c588cce78e749aba878580dac
Tharunjinka6 <tharunjinka2155@gmail.com>
2026-07-01 11:59:53 +0530

    Update Frequency counter.py

--- Frequency counter.py
+++ Frequency counter.py
@@ -1,10 +1,14 @@
 arr=[2,3,2,3,5]
 sorted_arr=sorted(arr)
-n=len(arr)
-new_list=[]
-for i in range(n-1):
-    for j in range(n-1):
-        arr[j]==arr[j+1]
-        new_list.append(arr[j])
-print(new_list)
-
+n=arr[-1]
+count_2=0
+count_3=0
+count_5=0
+for i in range(1,n+1):
+    if i==2:
+        count_2+=1
+    elif i==3:
+        count_3+=1
+    elif i==5:
+        count_5+=1
+print(count_2,count_3,count_5)
