cfe1c01cf5ad6fe187a7322c2934bef060b2db42
Tharunjinka6 <tharunjinka2155@gmail.com>
2026-07-13 18:45:53 +0530

    binary

---
+++ binary seasrch gfg.py
@@ -0,0 +1,14 @@
+class Solution:
+    def binarySearch(self, arr, k):
+        # code here
+        low=0
+        high=len(arr)-1
+        while low<=high:
+            mid=(low+high)//2
+            if arr[mid]==k:
+                return True
+            elif arr[mid]<k:
+                low=mid+1
+            else:
+                high=mid-1
+        return False
